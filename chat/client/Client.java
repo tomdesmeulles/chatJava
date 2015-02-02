022e369c9533a1ab3ac17807d57125b5454e9aac
Paul <paul@MacBook-Air-de-Paul.local>
2015-02-02 13:44:46 +0100

    ajout anciens fichiers chat

---
+++ chat/client/Client.java
@@ -0,0 +1,10 @@
+package fr.dnr2i.chat.client;
+
+public class Client
+{	
+	public Client()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ chat/server/ServerMain.java
@@ -0,0 +1,11 @@
+package fr.dnr2i.chat.server;
+
+public class ServerMain
+{
+
+	public ServerMain()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
